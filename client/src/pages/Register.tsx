82e9fc7cb1e4342ce254aaa67498d09cf1c77386
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-04-25 21:20:58 +0200

    Removed uncessesery comment

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,12 +1,14 @@
 import "./styles/register.scss";
 
 import { RegisterForm } from "features/authentication/index";
+import { BackButton } from "features/interface";
 
 export default function Register() {
   return (
     <main className="register-page">
       <h1>Create a new account</h1>
       <RegisterForm />
+      <BackButton />
     </main>
   );
 }
