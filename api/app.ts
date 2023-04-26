66cbe4d16ce7df47c6ab69076c8381630a638290
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-04-26 11:35:38 +0200

    Path in server

--- api/app.ts
+++ api/app.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import { connectToMongoDB } from "./middleware/connectToMongoDB";
 import { setUpSession } from "./middleware/setUpSession";
@@ -25,6 +26,7 @@ app.use(setUpSession);
 app.use(passportConfig);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Routes
 app.use("/users", usersRouter);
