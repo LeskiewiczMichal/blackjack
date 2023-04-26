e520453a358cd71595ae7b9c45e2b122b8dc3fd5
LeskiewiczMichal <leskiewicz02@gmail.com>
2023-04-26 12:22:38 +0200

    Deployment

--- api/dist/app.js
+++ api/dist/app.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const connectToMongoDB_1 = require("./middleware/connectToMongoDB");
 const setUpSession_1 = require("./middleware/setUpSession");
 const passportConfig_1 = require("./middleware/passportConfig");
@@ -15,16 +16,17 @@ const skins_1 = require("./routes/skins");
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 9000;
 // CORS for my localhost
-const corsOptions = {
-    origin: "http://localhost:3000",
-    credentials: true,
-};
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true,
+// };
 // Middleware
-app.use((0, cors_1.default)(corsOptions));
+app.use((0, cors_1.default)());
 app.use(setUpSession_1.setUpSession);
 app.use(passportConfig_1.passportConfig);
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(express_1.default.json());
+app.use(express_1.default.static(path_1.default.join(__dirname, "../client/build")));
 // Routes
 app.use("/users", users_1.usersRouter);
 app.use("/skins", skins_1.skinsRouter);
